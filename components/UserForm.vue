351f57b201ad589eb18f2bbc292803b698840c18
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-07-24 12:56:19 +0700

    user dipecah

---
+++ components/UserForm.vue
@@ -0,0 +1,126 @@
+<template>
+  <el-dialog
+    v-model="showForm"
+    width="500px"
+    :title="!!userStore.formModel.id ? 'EDIT USER' : 'ADD USER'"
+    :close-on-click-modal="false"
+  >
+    <el-form label-width="160px" label-position="left">
+      <el-form-item label="Name" :error="userStore.formErrors.name">
+        <el-input
+          placeholder="Name"
+          v-model="userStore.formModel.name"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item label="Email" :error="userStore.formErrors.email">
+        <el-input
+          placeholder="Email"
+          v-model="userStore.formModel.email"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item label="Password" :error="userStore.formErrors.password">
+        <el-input
+          type="password"
+          placeholder="Password"
+          v-model="userStore.formModel.password"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item label="Roles" :error="userStore.formErrors.roles">
+        <el-select
+          v-model="userStore.formModel.roles"
+          placeholder="Roles"
+          style="width: 100%"
+          multiple
+        >
+          <el-option
+            v-for="(role, i) in store.roles"
+            :value="role"
+            :label="role"
+            :key="i"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item
+        label="Department"
+        :error="userStore.formErrors.departmentId"
+      >
+        <el-select
+          v-model="userStore.formModel.departmentId"
+          placeholder="Department"
+          style="width: 100%"
+        >
+          <el-option
+            v-for="(el, i) in departments"
+            :value="el.id"
+            :label="`${el.code} - ${el.name}`"
+            :key="i"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="Bank" :error="userStore.formErrors.bankId">
+        <el-select
+          v-model="userStore.formModel.bankId"
+          placeholder="Bank"
+          style="width: 100%"
+        >
+          <el-option
+            v-for="(el, i) in banks"
+            :value="el.id"
+            :label="`${el.code} - ${el.name}`"
+            :key="i"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item
+        label="Bank Account"
+        :error="userStore.formErrors.bankAccount"
+      >
+        <el-input
+          placeholder="Bank Account"
+          v-model="userStore.formModel.bankAccount"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item label="Status" :error="userStore.formErrors.active">
+        <el-switch
+          :active-value="true"
+          :inactive-value="false"
+          v-model="userStore.formModel.active"
+          style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+        >
+        </el-switch>
+        <el-tag
+          :type="userStore.formModel.active ? 'success' : 'danger'"
+          size="small"
+          style="margin-left: 10px"
+          effect="dark"
+        >
+          {{ !!userStore.formModel.active ? "Aktif" : "Nonaktif" }}
+        </el-tag>
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <el-button :icon="CircleCloseFilled" @click="userStore.closeForm">
+        CANCEL
+      </el-button>
+      <el-button :icon="SuccessFilled" type="success" @click="userStore.save()">
+        SAVE
+      </el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import { SuccessFilled, CircleCloseFilled } from "@element-plus/icons-vue";
+const userStore = useUserStore();
+</script>
--- pages/master-data/users.vue
+++ pages/master-data/users.vue
@@ -1,8 +1,11 @@
 <template>
-  <form class="flex justify-content-end" @submit.prevent="searchData">
+  <form
+    class="flex justify-content-end"
+    @submit.prevent="userStore.requestData"
+  >
     <el-button
       size="small"
-      @click="openForm({ roles: ['USER'], password: '' })"
+      @click="userStore.openForm({ roles: ['USER'], password: '' })"
       type="success"
       :icon="Plus"
       class="mr-2"
@@ -11,18 +14,19 @@
     </el-button>
     <el-input
       size="small"
-      v-model="keyword"
+      v-model="userStore.keyword"
       placeholder="Cari"
       style="width: 180px"
       :prefix-icon="Search"
       :clearable="true"
+      @clear="userStore.requestData"
     >
     </el-input>
   </form>
 
   <br />
 
-  <el-table stripe v-loading="loading" :data="tableData">
+  <el-table stripe v-loading="userStore.loading" :data="userStore.users">
     <el-table-column type="index" label="#"></el-table-column>
 
     <el-table-column label="Name" min-width="150">
@@ -82,7 +86,8 @@
 
     <el-table-column width="60px" align="center" header-align="center">
       <template #header>
-        <el-button link @click="refreshData" :icon="Refresh"> </el-button>
+        <el-button link @click="userStore.requestData" :icon="Refresh">
+        </el-button>
       </template>
       <template #default="{ row }">
         <el-dropdown>
@@ -95,13 +100,13 @@
             <el-dropdown-menu>
               <el-dropdown-item
                 :icon="Edit"
-                @click.native.prevent="openForm(row)"
+                @click.native.prevent="userStore.openForm(row)"
               >
                 Edit
               </el-dropdown-item>
               <el-dropdown-item
                 :icon="Delete"
-                @click.native.prevent="deleteData(row.id, store.getUsers)"
+                @click.native.prevent="userStore.remove(row.id)"
               >
                 Delete
               </el-dropdown-item>
@@ -112,158 +117,21 @@
     </el-table-column>
   </el-table>
 
-  <el-dialog
-    v-model="showForm"
-    width="500px"
-    :title="!!formModel.id ? 'EDIT USER' : 'ADD USER'"
-    :close-on-click-modal="false"
-  >
-    <el-form label-width="160px" label-position="left">
-      <el-form-item label="Name" :error="formErrors.name">
-        <el-input placeholder="Name" v-model="formModel.name"></el-input>
-      </el-form-item>
-
-      <el-form-item label="Email" :error="formErrors.email">
-        <el-input placeholder="Email" v-model="formModel.email"></el-input>
-      </el-form-item>
-
-      <el-form-item label="Password" :error="formErrors.password">
-        <el-input
-          type="password"
-          placeholder="Password"
-          v-model="formModel.password"
-        ></el-input>
-      </el-form-item>
-
-      <el-form-item label="Roles" :error="formErrors.roles">
-        <el-select
-          v-model="formModel.roles"
-          placeholder="Roles"
-          style="width: 100%"
-          multiple
-        >
-          <el-option
-            v-for="(role, i) in store.roles"
-            :value="role"
-            :label="role"
-            :key="i"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="Department" :error="formErrors.departmentId">
-        <el-select
-          v-model="formModel.departmentId"
-          placeholder="Department"
-          style="width: 100%"
-        >
-          <el-option
-            v-for="(el, i) in departments"
-            :value="el.id"
-            :label="`${el.code} - ${el.name}`"
-            :key="i"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="Bank" :error="formErrors.bankId">
-        <el-select
-          v-model="formModel.bankId"
-          placeholder="Bank"
-          style="width: 100%"
-        >
-          <el-option
-            v-for="(el, i) in banks"
-            :value="el.id"
-            :label="`${el.code} - ${el.name}`"
-            :key="i"
-          >
-          </el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="Bank Account" :error="formErrors.bankAccount">
-        <el-input
-          placeholder="Bank Account"
-          v-model="formModel.bankAccount"
-        ></el-input>
-      </el-form-item>
-
-      <el-form-item label="Status" :error="formErrors.active">
-        <el-switch
-          :active-value="true"
-          :inactive-value="false"
-          v-model="formModel.active"
-          style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
-        >
-        </el-switch>
-        <el-tag
-          :type="formModel.active ? 'success' : 'danger'"
-          size="small"
-          style="margin-left: 10px"
-          effect="dark"
-        >
-          {{ !!formModel.active ? "Aktif" : "Nonaktif" }}
-        </el-tag>
-      </el-form-item>
-    </el-form>
-
-    <template #footer>
-      <el-button :icon="CircleCloseFilled" @click="closeForm">
-        CANCEL
-      </el-button>
-      <el-button
-        :icon="SuccessFilled"
-        type="success"
-        @click="save(store.getUsers)"
-      >
-        SAVE
-      </el-button>
-    </template>
-  </el-dialog>
+  <UserForm />
 </template>
 
 <script setup>
-const store = useWebsiteStore();
-
 import {
   Refresh,
   Plus,
-  SuccessFilled,
-  CircleCloseFilled,
   Edit,
   Delete,
   MoreFilled,
-  Search,
 } from "@element-plus/icons-vue";
 
-const departments = computed(() => store.departments);
-const banks = computed(() => store.banks);
-
-const {
-  showForm,
-  formErrors,
-  formModel,
-  tableData,
-  loading,
-  keyword,
-  openForm,
-  save,
-  deleteData,
-  closeForm,
-  requestData,
-  searchData,
-  refreshData,
-} = useCrud("/api/users");
+const userStore = useUserStore();
 
 onMounted(() => {
-  requestData();
-});
-
-onBeforeMount(async () => {
-  await store.getDepartments();
-  await store.getBanks();
+  userStore.requestData();
 });
 </script>
