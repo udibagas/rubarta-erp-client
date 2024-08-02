74afeb54c9a33e2b76e1c9db8497775e8d45ef55
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-02 10:54:55 +0700

    approval detail

---
+++ components/ApprovalDetail.vue
@@ -0,0 +1,91 @@
+<template>
+  <div
+    style="
+      display: flex;
+      align-items: stretch;
+      justify-content: space-evenly;
+      text-align: center;
+      border: 1px solid #ddd;
+      padding: 20px;
+      border-radius: 5px;
+    "
+  >
+    <div v-for="approval in approvalList" :key="approval.id">
+      <strong>{{ actions[approval.approvalActionType] }}</strong>
+      <div style="height: 60px; line-height: 60px">
+        <el-button
+          type="danger"
+          :icon="Stamp"
+          @click="approve(requestId)"
+          v-if="user.id == approval.userId && approval.approvalStatus === null"
+        >
+          APPROVE
+        </el-button>
+
+        <div v-else>
+          <img
+            v-if="approval.approvalStatus && approval.User.signatureSpeciment"
+            :src="`${config.public.apiBase}/${approval.User.signatureSpeciment.filePath}`"
+            alt=""
+            style="height: 60px"
+          />
+          <el-tag
+            v-else
+            :type="approval.approvalStatus === null ? 'info' : 'success'"
+            effect="dark"
+          >
+            {{ approval.approvalStatus === null ? "PENDING" : "APPROVED" }}
+          </el-tag>
+        </div>
+      </div>
+
+      <div class="strong">
+        {{ approval.User?.name }}
+      </div>
+      <div>
+        {{ approval.approvalStatus ? formatDateTime(approval.updatedAt) : "-" }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Stamp } from "@element-plus/icons-vue";
+const config = useRuntimeConfig();
+const queryClient = useQueryClient();
+const { user } = useSanctumAuth();
+const request = useRequest();
+const emit = defineEmits(["reload"]);
+const { approvals, queryKey, approveUrl, requestId } = defineProps([
+  "approvals",
+  "queryKey",
+  "approveUrl",
+  "requestId",
+]);
+
+const approvalList = toRef(approvals);
+
+const actions = {
+  APPROVAL: "APPROVED BY",
+  VERIFICATION: "VERIFIED BY",
+  AUTHORIZATION: "AUTHORIZED BY",
+};
+
+async function approve(id) {
+  try {
+    await ElMessageBox.confirm(
+      "Anda yakin akan melakukan persetujuan?",
+      "Warning",
+      {
+        type: "warning",
+      }
+    );
+
+    await request(`${approveUrl}/${id}`, { method: "POST" });
+    emit("reload");
+    queryClient.invalidateQueries({ queryKey: [queryKey] });
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
