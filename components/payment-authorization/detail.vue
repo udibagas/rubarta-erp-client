5c61f52cbc9281139b3657bb9c414f9cdccf7c1a
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-03 20:47:23 +0700

    pisahin approval

--- components/payment-authorization/detail.vue
+++ components/payment-authorization/detail.vue
@@ -102,59 +102,14 @@
 
     <br />
 
-    <div
-      v-if="detail.PaymentAuthorizationApproval?.length"
-      style="
-        display: flex;
-        align-items: stretch;
-        justify-content: space-evenly;
-        text-align: center;
-        border: 1px solid #ddd;
-        padding: 20px;
-        border-radius: 5px;
-      "
-    >
-      <div v-for="approval in detail.PaymentAuthorizationApproval">
-        <strong>{{ actions[approval.approvalActionType] }}</strong>
-        <div style="height: 60px; line-height: 60px">
-          <el-button
-            type="danger"
-            :icon="Stamp"
-            @click="approve(approval.id)"
-            v-if="
-              user.id == approval.userId && approval.approvalStatus === null
-            "
-          >
-            APPROVE
-          </el-button>
-
-          <div v-else>
-            <img
-              v-if="approval.approvalStatus && approval.User.signatureSpeciment"
-              :src="`${config.public.apiBase}/${approval.User.signatureSpeciment.filePath}`"
-              alt=""
-              style="height: 60px"
-            />
-            <el-tag
-              v-else
-              :type="approval.approvalStatus === null ? 'info' : 'success'"
-              effect="dark"
-            >
-              {{ approval.approvalStatus === null ? "PENDING" : "APPROVED" }}
-            </el-tag>
-          </div>
-        </div>
-
-        <div class="strong">
-          {{ approval.User?.name }}
-        </div>
-        <div>
-          {{
-            approval.approvalStatus ? formatDateLong(approval.updatedAt) : "-"
-          }}
-        </div>
-      </div>
-    </div>
+    <ApprovalDetail
+      v-if="detail.PaymentAuthorizationApproval.length > 0"
+      approve-url="/api/payment-authorizations/approve"
+      query-key="payment-authorizations"
+      :approvals="detail.PaymentAuthorizationApproval"
+      :request-id="detail.id"
+      @reload="reload"
+    />
 
     <template #footer>
       <el-button :icon="CircleCloseFilled" @click="closeDetail">
@@ -169,58 +124,57 @@
       >
         EDIT
       </el-button>
+
+      <el-button
+        v-if="detail.status == 'DRAFT'"
+        :icon="SuccessFilled"
+        type="success"
+        @click="submit(detail.id)"
+      >
+        SUBMIT
+      </el-button>
     </template>
   </el-dialog>
 </template>
 
 <script setup>
-import {
-  SuccessFilled,
-  CircleCloseFilled,
-  Stamp,
-} from "@element-plus/icons-vue";
+import { SuccessFilled, CircleCloseFilled } from "@element-plus/icons-vue";
 import { showDetail, detail, closeDetail } from "~/stores/detail";
 import { colors } from "~/constants/colors";
 import { openForm } from "~/stores/form";
 
-const { user } = useSanctumAuth();
 const request = useRequest();
 const queryClient = useQueryClient();
-const config = useRuntimeConfig();
 
-const actions = {
-  APPROVAL: "APPROVED BY",
-  VERIFICATION: "VERIFIED BY",
-  AUTHORIZATION: "AUTHORIZED BY",
-};
+function reload() {
+  request(`/api/expense-claims/${detail.value.id}`).then(
+    (res) => (detail.value = res)
+  );
+}
 
-async function approve(id) {
+function edit(data) {
+  closeDetail();
+  openForm(data);
+}
+
+async function submit(id) {
   try {
     await ElMessageBox.confirm(
-      "Anda yakin akan melakukan persetujuan?",
+      "Anda yakin akan mengajukan klaim ini?",
       "Warning",
       {
         type: "warning",
       }
     );
 
-    const response = await request(
-      `/api/payment-authorizations/approve/${detail.value.id}`,
-      { method: "POST" }
-    );
-
-    detail.value = response;
-    queryClient.invalidateQueries({
-      queryKey: ["payment-authorizations"],
+    await request(`/api/payment-authorizations/submit/${id}`, {
+      method: "POST",
     });
+    queryClient.invalidateQueries({ queryKey: ["payment-authorizations"] });
+    closeDetail();
   } catch (error) {
-    console.log(error);
+    return;
   }
-}
-
-function edit(data) {
-  closeDetail();
-  openForm(data);
 }
 </script>
 
