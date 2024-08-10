f9e050412fab055e6da6018b882d9c69c29e6f47
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-08-10 16:21:05 +0700

    bikin sub menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -5,14 +5,23 @@
     :default-active="$route.path"
     class="sidebar"
   >
-    <el-menu-item
-      v-for="(m, i) in links.filter((l) => l.visible)"
-      :index="m.path"
-      :key="i"
-    >
-      <el-icon><component :is="icon[m.icon]" /></el-icon>
-      <span slot="title"> {{ m.label }} </span>
-    </el-menu-item>
+    <template v-for="(m, i) in links.filter((l) => l.visible)" :key="i">
+      <el-sub-menu v-if="m.children">
+        <template #title>
+          <el-icon><component :is="m.icon"></component> </el-icon>
+          <span slot="title"> {{ m.label }} </span>
+        </template>
+        <el-menu-item v-for="(sm, j) in m.children" :index="sm.path">
+          <el-icon><component :is="sm.icon"></component> </el-icon>
+          <span slot="title"> {{ sm.label }} </span>
+        </el-menu-item>
+      </el-sub-menu>
+
+      <el-menu-item :index="m.path" v-else>
+        <el-icon><component :is="m.icon"></component> </el-icon>
+        <span slot="title"> {{ m.label }} </span>
+      </el-menu-item>
+    </template>
   </el-menu>
 </template>
 
@@ -20,70 +29,78 @@
 const { user } = useSanctumAuth();
 const { collapse } = defineProps(["collapse"]);
 
-import {
-  User,
-  HomeFilled,
-  Operation,
-  DocumentCopy,
-  CreditCard,
-  DataAnalysis,
-  Setting,
-  Coin,
-  Memo,
-  DataLine,
-  Stamp,
-} from "@element-plus/icons-vue";
-
-const icon = {
-  User,
-  HomeFilled,
-  Operation,
-  DocumentCopy,
-  CreditCard,
-  DataAnalysis,
-  Setting,
-  Coin,
-  Memo,
-  DataLine,
-  Stamp,
-};
-
 const links = [
   {
     label: "Dashboard",
     path: "/",
-    icon: "DataLine",
+    icon: ElIconDataLine,
     visible: true,
   },
   {
     label: "NKP",
     path: "/nkp",
-    icon: "Stamp",
+    icon: ElIconStamp,
     visible: !(user.value.roles.length == 1 && user.value.roles[0] == "USER"),
   },
   {
     label: "NKP Declaration",
     path: "/nkp-declaration",
-    icon: "DocumentCopy",
+    icon: ElIconDocumentCopy,
     visible: true,
   },
   {
     label: "Expense Notes",
     path: "/expense-notes",
-    icon: "Memo",
+    icon: ElIconMemo,
     visible: true,
   },
   {
     label: "Report",
     path: "/report",
-    icon: "Memo",
+    icon: ElIconPieChart,
     visible: true,
   },
   {
     label: "Master Data",
-    path: "/master-data/companies",
-    icon: "Coin",
+    icon: ElIconCoin,
     visible: user.value.roles.includes("ADMIN"),
+    children: [
+      {
+        label: "Companies",
+        path: "/master-data/companies",
+        icon: ElIconOfficeBuilding,
+      },
+      {
+        label: "Departments",
+        path: "/master-data/departments",
+        icon: ElIconMenu,
+      },
+      {
+        label: "Banks",
+        path: "/master-data/banks",
+        icon: ElIconMoney,
+      },
+      {
+        label: "Vendors",
+        path: "/master-data/suppliers",
+        icon: ElIconConnection,
+      },
+      {
+        label: "Expense Types",
+        path: "/master-data/expense-types",
+        icon: ElIconPriceTag,
+      },
+      {
+        label: "Users",
+        path: "/master-data/users",
+        icon: ElIconUser,
+      },
+      {
+        label: "Approval Setting",
+        path: "/master-data/approval-setting",
+        icon: ElIconOperation,
+      },
+    ],
   },
 ];
 </script>
