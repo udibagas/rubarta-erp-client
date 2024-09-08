19c867519b951ea7a488b05e0f68690842126b38
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-09-08 20:32:55 +0700

    update

--- components/StatusTag.vue
+++ components/StatusTag.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-tag :type="colors[status]" :effect="effect" round>
+  <el-tag :type="colors[status]" effect="dark" round>
     {{ status.replace("_", " ") }}
   </el-tag>
 </template>
