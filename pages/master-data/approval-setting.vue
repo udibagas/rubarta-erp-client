1651d5cab0318785d1e490d4f7d85678f0fe9f69
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-10-15 20:49:15 +0700

    use table

--- pages/master-data/approval-setting.vue
+++ pages/master-data/approval-setting.vue
@@ -24,68 +24,72 @@
 
   <br />
 
-  <el-row type="flex" :gutter="10">
-    <el-col v-for="d in data" :key="d.id" :lg="8" :md="12" :sm="24">
-      <el-card :body-style="{ padding: '5px' }">
-        <el-descriptions border :column="1">
-          <el-descriptions-item label="Company">
-            <div
-              style="
-                display: flex;
-                gap: 5px;
-                align-items: center;
-                justify-content: space-between;
-              "
-            >
-              <strong>{{ d.Company.name }}</strong>
-              <div>
-                <el-button
-                  link
-                  :icon="ElIconEdit"
-                  @click="openForm(d)"
-                ></el-button>
-                <el-button
-                  type="danger"
-                  link
-                  :icon="ElIconDelete"
-                  @click="handleRemove(d.id, remove)"
-                ></el-button>
-              </div>
-            </div>
-          </el-descriptions-item>
-          <el-descriptions-item label="Approval Type">
-            {{ d.approvalType }}
-          </el-descriptions-item>
-          <el-descriptions-item label="Payment Target">
-            {{ d.paymentType }}
-          </el-descriptions-item>
-          <el-descriptions-item label="NKP Type">
-            {{ d.nkpType?.replace("_", " ") }}
-          </el-descriptions-item>
-        </el-descriptions>
+  <el-table stripe v-loading="isPending" :data="data">
+    <el-table-column type="index" label="#"></el-table-column>
 
-        <div :style="{ margin: '20px 10px 10px 10px' }">
-          <el-steps direction="vertical" :space="40">
-            <el-step
-              v-for="item in d.ApprovalSettingItem"
-              :key="item.id"
-              :title="item.User.name"
-              :status="'wait'"
-            ></el-step>
-          </el-steps>
+    <el-table-column label="Company" width="220">
+      <template #default="{ row }">
+        {{ row.Company.name }}
+      </template>
+    </el-table-column>
+    <el-table-column prop="approvalType" label="Approval Type" width="150" />
+    <el-table-column prop="paymentType" label="Payment Target" width="220" />
+    <el-table-column label="Payment Type" width="220">
+      <template #default="{ row }">
+        {{ row.nkpType.replace("_", " ") }}
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Approvals">
+      <template #default="{ row }">
+        <div v-for="(item, i) in row.ApprovalSettingItem" :key="i">
+          [{{ item.level }}] {{ item.approvalActionType }} -
+          {{ item.User?.name }}
         </div>
-      </el-card>
-    </el-col>
-  </el-row>
+      </template>
+    </el-table-column>
+
+    <el-table-column width="60px" align="center" header-align="center">
+      <template #header>
+        <el-button link @click="refreshData" :icon="ElIconRefresh"> </el-button>
+      </template>
+      <template #default="{ row }">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            <el-icon>
+              <ElIconMoreFilled />
+            </el-icon>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item
+                :icon="ElIconEdit"
+                @click.native.prevent="openForm(row)"
+              >
+                Edit
+              </el-dropdown-item>
+              <el-dropdown-item
+                :icon="ElIconDelete"
+                @click.native.prevent="handleRemove(row.id, remove)"
+              >
+                Delete
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </template>
+    </el-table-column>
+  </el-table>
 
   <ApprovalSettingForm />
 </template>
 
 <script setup>
-const { openForm, removeMutation, fetchData, handleRemove } = useCrud({
-  url: "/api/approval-settings",
-  queryKey: "approval-settings",
-});
+const { openForm, removeMutation, fetchData, refreshData, handleRemove } =
+  useCrud({
+    url: "/api/approval-settings",
+    queryKey: "approval-settings",
+  });
 
 const { isPending, data } = fetchData();
 const { mutate: remove } = removeMutation();
