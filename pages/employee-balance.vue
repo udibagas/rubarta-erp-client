3bd3eb2bbc04945a61c3fbab46e81f62036510b4
udibagas <udibagas@gmail.com>
2026-07-27 08:44:29 +0700

    feat: enhance employee balance table with avatar and formatting improvements

--- pages/employee-balance.vue
+++ pages/employee-balance.vue
@@ -3,19 +3,53 @@
 
   <br />
 
-  <el-table :data="data" v-loading="isPending" stripe>
+  <el-table
+    :data="data"
+    v-loading="isPending"
+    stripe
+    height="calc(100vh - 168px)"
+  >
     <el-table-column label="Employee">
-      <template #default="{ row }"> {{ row.user.name }} </template>
+      <template #default="{ row }">
+        <div class="flex items-center gap-2">
+          <el-avatar
+            v-if="row.user.avatar"
+            :src="row.user.avatar"
+            size="small"
+          />
+          <el-avatar
+            v-else
+            size="small"
+            class="shrink-0"
+            :style="{ backgroundColor: getAvatarColor(row.user.name) }"
+          >
+            {{ row.user.name?.charAt(0).toUpperCase() }}
+          </el-avatar>
+          <span class="font-semibold text-sm line-clamp-1">
+            {{ row.user.name }}
+          </span>
+        </div>
+      </template>
     </el-table-column>
 
-    <el-table-column label="Balance">
-      <template #default="{ row }"> {{ toRupiah(row.balance) }} </template>
+    <el-table-column label="Balance" align="right">
+      <template #default="{ row }">
+        <el-tag type="success" effect="plain" class="font-mono">
+          {{ toRupiah(row.balance) }}
+        </el-tag>
+      </template>
     </el-table-column>
 
-    <el-table-column label="Description" prop="description"></el-table-column>
+    <el-table-column label="Description" prop="description" min-width="150">
+      <template #default="{ row }">
+        <span class="font-mono line-clamp-1">{{ row.description }}</span>
+      </template>
+    </el-table-column>
 
     <el-table-column label="Last Update">
-      <template #default="{ row }"> {{ formatDate(row.updatedAt) }} </template>
+      <template #default="{ row }">
+        {{ formatDate(row.updatedAt) }} {{ formatTime(row.updatedAt) }}
+      </template>
     </el-table-column>
   </el-table>
 </template>
