f08af65c81642475324ea691102f3216def08fca
udibagas <udibagas@gmail.com>
2026-07-27 18:08:41 +0700

    small avatar

--- pages/crm/leads/index.vue
+++ pages/crm/leads/index.vue
@@ -130,7 +130,7 @@
         <template #default="{ row }">
           <div v-if="row.User" class="flex items-center gap-2">
             <el-avatar
-              :size="30"
+              size="small"
               class="shrink-0"
               :style="{ backgroundColor: getAvatarColor(row.User.name) }"
             >
