1b9a1dfb8d36a64b87a58e3156c3a85e8df5dc23
udibagas <udibagas@gmail.com>
2026-07-27 15:56:25 +0700

    benerin layout

--- pages/master-data/approval-setting.vue
+++ pages/master-data/approval-setting.vue
@@ -1,89 +1,101 @@
 <template>
-  <el-page-header @back="goBack" content="Approval Setting">
-    <template #extra>
-      <el-button
-        :icon="ElIconPlus"
-        type="success"
-        @click="
-          openForm({
-            ApprovalSettingItem: [
-              {
-                level: undefined,
-                approvalActionType: undefined,
-                userId: undefined,
-              },
-            ],
-          })
-        "
-      >
-        ADD APPROVAL SETTING
-      </el-button>
+  <nuxt-layout name="default">
+    <template #header>
+      <el-page-header @back="goBack" content="Approval Setting">
+        <template #extra>
+          <el-button
+            :icon="ElIconPlus"
+            type="success"
+            @click="
+              openForm({
+                ApprovalSettingItem: [
+                  {
+                    level: undefined,
+                    approvalActionType: undefined,
+                    userId: undefined,
+                  },
+                ],
+              })
+            "
+          >
+            ADD APPROVAL SETTING
+          </el-button>
+        </template>
+      </el-page-header>
     </template>
-  </el-page-header>
 
-  <br />
+    <el-table
+      stripe
+      v-loading="isPending"
+      :data="data"
+      height="calc(100vh - 155px)"
+    >
+      <el-table-column type="index" label="#"></el-table-column>
 
-  <el-table stripe v-loading="isPending" :data="data">
-    <el-table-column type="index" label="#"></el-table-column>
+      <el-table-column label="Company" width="220">
+        <template #default="{ row }">
+          {{ row.Company.name }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="approvalType" label="Approval Type" width="150" />
+      <el-table-column prop="paymentType" label="Payment Target" width="220" />
+      <el-table-column label="Payment Type" width="220">
+        <template #default="{ row }">
+          {{ row.nkpType.replace("_", " ") }}
+        </template>
+      </el-table-column>
 
-    <el-table-column label="Company" width="220">
-      <template #default="{ row }">
-        {{ row.Company.name }}
-      </template>
-    </el-table-column>
-    <el-table-column prop="approvalType" label="Approval Type" width="150" />
-    <el-table-column prop="paymentType" label="Payment Target" width="220" />
-    <el-table-column label="Payment Type" width="220">
-      <template #default="{ row }">
-        {{ row.nkpType.replace("_", " ") }}
-      </template>
-    </el-table-column>
+      <el-table-column label="Approvals">
+        <template #default="{ row }">
+          <div v-for="(item, i) in row.ApprovalSettingItem" :key="i">
+            [{{ item.level }}] {{ item.approvalActionType }} -
+            {{ item.User?.name }}
+          </div>
+        </template>
+      </el-table-column>
 
-    <el-table-column label="Approvals">
-      <template #default="{ row }">
-        <div v-for="(item, i) in row.ApprovalSettingItem" :key="i">
-          [{{ item.level }}] {{ item.approvalActionType }} -
-          {{ item.User?.name }}
-        </div>
-      </template>
-    </el-table-column>
+      <el-table-column width="60px" align="center" header-align="center">
+        <template #header>
+          <el-button link @click="refreshData" :icon="ElIconRefresh">
+          </el-button>
+        </template>
+        <template #default="{ row }">
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <el-icon>
+                <ElIconMoreFilled />
+              </el-icon>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item
+                  :icon="ElIconEdit"
+                  @click.native.prevent="openForm(row)"
+                >
+                  Edit
+                </el-dropdown-item>
+                <el-dropdown-item
+                  :icon="ElIconDelete"
+                  @click.native.prevent="handleRemove(row.id, remove)"
+                >
+                  Delete
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </template>
+      </el-table-column>
+    </el-table>
 
-    <el-table-column width="60px" align="center" header-align="center">
-      <template #header>
-        <el-button link @click="refreshData" :icon="ElIconRefresh"> </el-button>
-      </template>
-      <template #default="{ row }">
-        <el-dropdown>
-          <span class="el-dropdown-link">
-            <el-icon>
-              <ElIconMoreFilled />
-            </el-icon>
-          </span>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item
-                :icon="ElIconEdit"
-                @click.native.prevent="openForm(row)"
-              >
-                Edit
-              </el-dropdown-item>
-              <el-dropdown-item
-                :icon="ElIconDelete"
-                @click.native.prevent="handleRemove(row.id, remove)"
-              >
-                Delete
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </template>
-    </el-table-column>
-  </el-table>
-
-  <ApprovalSettingForm />
+    <ApprovalSettingForm />
+  </nuxt-layout>
 </template>
 
 <script setup>
+definePageMeta({
+  layout: false,
+});
+
 const {
   openForm,
   removeMutation,
--- pages/master-data/users.vue
+++ pages/master-data/users.vue
@@ -1,125 +1,135 @@
 <template>
-  <el-page-header @back="goBack" content="Employees">
-    <template #extra>
-      <div class="flex gap-2">
-        <el-button
-          @click="openForm({ roles: ['USER'], password: '' })"
-          type="success"
-          :icon="ElIconPlus"
-        >
-          ADD NEW EMPLOYEE
-        </el-button>
+  <nuxt-layout name="default">
+    <template #header>
+      <el-page-header @back="goBack" content="Employees">
+        <template #extra>
+          <div class="flex gap-2">
+            <el-button
+              @click="openForm({ roles: ['USER'], password: '' })"
+              type="success"
+              :icon="ElIconPlus"
+            >
+              ADD NEW EMPLOYEE
+            </el-button>
 
-        <el-input
-          placeholder="Seach Employee"
-          v-model="keyword"
-          clearable
-          class="w-48!"
-          :prefix-icon="ElIconSearch"
-          @change="() => refreshData()"
-          @clear="() => refreshData()"
-        />
-      </div>
+            <el-input
+              placeholder="Seach Employee"
+              v-model="keyword"
+              clearable
+              class="w-48!"
+              :prefix-icon="ElIconSearch"
+              @change="() => refreshData()"
+              @clear="() => refreshData()"
+            />
+          </div>
+        </template>
+      </el-page-header>
     </template>
-  </el-page-header>
-
-  <br />
-
-  <el-table stripe v-loading="isPending" :data="data">
-    <el-table-column type="index" label="#"></el-table-column>
-
-    <el-table-column label="Name" min-width="150">
-      <template #default="{ row }">
-        <strong>{{ row.name }}</strong>
-        <br />
-        {{ row.email }}
-      </template>
-    </el-table-column>
-
-    <el-table-column label="Department" min-width="150">
-      <template #default="{ row }">
-        {{ row.Department?.name }} <br />
-        <el-tag
-          v-for="role in row.roles"
-          :key="role"
-          type="info"
-          size="small"
-          effect="dark"
-          class="mr-1"
-        >
-          {{ role }}
-        </el-tag>
-      </template>
-    </el-table-column>
-
-    <el-table-column label="Bank" min-width="150">
-      <template #default="{ row }">
-        {{ row.Bank?.name }} <br />
-        {{ row.bankAccount }} ({{ row.currency }})
-      </template>
-    </el-table-column>
-
-    <el-table-column
-      prop="status"
-      label="Status"
-      align="center"
-      header-align="center"
-      width="100"
+    <el-table
+      stripe
+      v-loading="isPending"
+      :data="data"
+      height="calc(100vh - 155px)"
     >
-      <template #default="{ row }">
-        <el-tag
-          :type="row.active ? 'success' : 'danger'"
-          size="small"
-          style="width: 100%"
-          effect="dark"
-        >
-          {{ row.active ? "Aktif" : "Nonaktif" }}
-        </el-tag>
-      </template>
-    </el-table-column>
+      <el-table-column type="index" label="#"></el-table-column>
 
-    <el-table-column
-      width="60px"
-      align="center"
-      header-align="center"
-      fixed="right"
-    >
-      <template #header>
-        <el-button link @click="refreshData()" :icon="ElIconRefresh">
-        </el-button>
-      </template>
-      <template #default="{ row }">
-        <el-dropdown>
-          <span class="el-dropdown-link">
-            <el-icon>
-              <ElIconMoreFilled />
-            </el-icon>
-          </span>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item
-                :icon="ElIconEdit"
-                @click.native.prevent="openForm(row)"
-              >
-                Edit
-              </el-dropdown-item>
-              <el-dropdown-item
-                :icon="ElIconDelete"
-                @click.native.prevent="handleRemove(row.id, remove)"
-              >
-                Delete
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-      </template>
-    </el-table-column>
-  </el-table>
+      <el-table-column label="Name" min-width="150">
+        <template #default="{ row }">
+          <strong>{{ row.name }}</strong>
+          <br />
+          {{ row.email }}
+        </template>
+      </el-table-column>
 
-  <UserForm />
+      <el-table-column label="Department" min-width="150">
+        <template #default="{ row }">
+          {{ row.Department?.name }} <br />
+          <el-tag
+            v-for="role in row.roles"
+            :key="role"
+            type="info"
+            size="small"
+            effect="dark"
+            class="mr-1"
+          >
+            {{ role }}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="Bank" min-width="150">
+        <template #default="{ row }">
+          {{ row.Bank?.name }} <br />
+          {{ row.bankAccount }} ({{ row.currency }})
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        prop="status"
+        label="Status"
+        align="center"
+        header-align="center"
+        width="100"
+      >
+        <template #default="{ row }">
+          <el-tag
+            :type="row.active ? 'success' : 'danger'"
+            size="small"
+            style="width: 100%"
+            effect="dark"
+          >
+            {{ row.active ? "Aktif" : "Nonaktif" }}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        width="60px"
+        align="center"
+        header-align="center"
+        fixed="right"
+      >
+        <template #header>
+          <el-button link @click="refreshData()" :icon="ElIconRefresh">
+          </el-button>
+        </template>
+        <template #default="{ row }">
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              <el-icon>
+                <ElIconMoreFilled />
+              </el-icon>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item
+                  :icon="ElIconEdit"
+                  @click.native.prevent="openForm(row)"
+                >
+                  Edit
+                </el-dropdown-item>
+                <el-dropdown-item
+                  :icon="ElIconDelete"
+                  @click.native.prevent="handleRemove(row.id, remove)"
+                >
+                  Delete
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <UserForm />
+  </nuxt-layout>
 </template>
 
 <script setup>
+definePageMeta({
+  layout: false,
+});
+
 const {
   openForm,
   removeMutation,
