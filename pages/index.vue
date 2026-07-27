fc412382e9ca2bece6b8cfb10e569cf4c6b658c3
udibagas <udibagas@gmail.com>
2026-07-27 15:34:17 +0700

    add bg

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,74 @@
 <template>
-  <el-row v-if="isSuccess" :gutter="10">
-    <el-col :span="6">
-      <el-card>
-        <el-statistic :value="summary.nkpDraft">
-          <template #title>
-            <div
-              style="
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-              "
-            >
-              <div class="title">NKP</div>
-              <StatusTag status="DRAFT" />
-            </div>
-          </template>
-        </el-statistic>
-      </el-card>
-    </el-col>
+  <div class="h-[calc(100vh-87px)] bg-white">
+    <el-row v-if="isSuccess" :gutter="10">
+      <el-col :span="6">
+        <el-card>
+          <el-statistic :value="summary.nkpDraft">
+            <template #title>
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                "
+              >
+                <div class="title">NKP</div>
+                <StatusTag status="DRAFT" />
+              </div>
+            </template>
+          </el-statistic>
+        </el-card>
+      </el-col>
 
-    <el-col :span="6">
-      <el-card>
-        <el-statistic :value="summary.nkpOpen">
-          <template #title>
-            <div
-              style="
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-              "
-            >
-              <div class="title">NKP</div>
-              <StatusTag status="OPEN" />
-            </div>
-          </template>
-        </el-statistic>
-      </el-card>
-    </el-col>
+      <el-col :span="6">
+        <el-card>
+          <el-statistic :value="summary.nkpOpen">
+            <template #title>
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                "
+              >
+                <div class="title">NKP</div>
+                <StatusTag status="OPEN" />
+              </div>
+            </template>
+          </el-statistic>
+        </el-card>
+      </el-col>
 
-    <el-col :span="6">
-      <el-card>
-        <el-statistic :value="summary.nkpClosed">
-          <template #title>
-            <div
-              style="
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-              "
-            >
-              <div class="title">NKP</div>
-              <StatusTag status="CLOSED" />
-            </div>
-          </template>
-        </el-statistic>
-      </el-card>
-    </el-col>
+      <el-col :span="6">
+        <el-card>
+          <el-statistic :value="summary.nkpClosed">
+            <template #title>
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                "
+              >
+                <div class="title">NKP</div>
+                <StatusTag status="CLOSED" />
+              </div>
+            </template>
+          </el-statistic>
+        </el-card>
+      </el-col>
 
-    <el-col :span="6">
-      <el-card>
-        <el-statistic :value="summary.pendingApprovalCount">
-          <template #title>
-            <div class="title">Pending Approval</div>
-          </template>
-        </el-statistic>
-      </el-card>
-    </el-col>
-  </el-row>
+      <el-col :span="6">
+        <el-card>
+          <el-statistic :value="summary.pendingApprovalCount">
+            <template #title>
+              <div class="title">Pending Approval</div>
+            </template>
+          </el-statistic>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script setup>
