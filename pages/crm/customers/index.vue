23a7b34ddb55f99d5e9ecffe123487937644fdf3
udibagas <udibagas@gmail.com>
2026-07-28 08:24:03 +0700

    perbaiki tampilan customer

--- pages/crm/customers/index.vue
+++ pages/crm/customers/index.vue
@@ -22,16 +22,41 @@
       stripe
       v-loading="isPending"
       :data="data"
-      @row-click="handleRowClick"
-      style="cursor: pointer"
       height="calc(100vh - 155px)"
     >
       <el-table-column type="index" label="#"></el-table-column>
 
-      <el-table-column label="Name" prop="name" min-width="150" />
-      <el-table-column label="Email" prop="email" min-width="150" />
-      <el-table-column label="Phone" prop="phone" min-width="150" />
-      <el-table-column label="Address" prop="address" min-width="200" />
+      <el-table-column label="Name" prop="name" min-width="150">
+        <template #default="{ row }">
+          <div class="font-semibold">
+            {{ row.name }}
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="Email" prop="email" min-width="150">
+        <template #default="{ row }">
+          <div class="flex items-center gap-1">
+            <el-icon><ElIconMessage /></el-icon>
+            <el-link type="success" :href="`mailto:${row.email}`">
+              {{ row.email }}
+            </el-link>
+          </div>
+          <div class="flex items-center gap-1">
+            <el-icon><ElIconPhone /></el-icon>
+            <span>
+              {{ row.phone }}
+            </span>
+          </div>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="Address" prop="address" min-width="200">
+        <template #default="{ row }">
+          <div class="whitespace-pre-line">
+            {{ row.address }}
+          </div>
+        </template>
+      </el-table-column>
 
       <el-table-column
         width="60px"
