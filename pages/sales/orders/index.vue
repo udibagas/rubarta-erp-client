cbfd2be119818597b0938720be6deebde8af339f
udibagas <udibagas@gmail.com>
2026-09-14 20:59:09 +0700

    fix item count

--- pages/sales/orders/index.vue
+++ pages/sales/orders/index.vue
@@ -92,7 +92,7 @@
             {{ toCurrency(row.grandTotal, row.currency) }}
           </div>
           <span class="text-xs text-gray-400">
-            {{ toDecimal(row._count.OrderItems) }} items
+            {{ toDecimal(row._count.SalesOrderItems) }} items
           </span>
         </template>
       </el-table-column>
