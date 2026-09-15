14a8303b2e4141e8cd1816408a1494cee3b5f389
udibagas <udibagas@gmail.com>
2026-09-15 15:22:41 +0700

    perbaiki dikit

--- pages/crm/contacts.vue
+++ pages/crm/contacts.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-layout name="default">
     <template #header>
-      <el-page-header @back="goBack" content="CRM / Contacts">
+      <el-page-header @back="goBack" content="Contacts">
         <template #extra>
           <div class="flex gap-2">
             <el-input
@@ -24,21 +24,27 @@
       :data="data"
       height="calc(100vh - 155px)"
     >
-      <el-table-column type="index" label="#"></el-table-column>
-
+      <template #empty>
+        <el-empty description="No Items"> </el-empty>
+      </template>
       <el-table-column label="Name" prop="name" min-width="150">
         <template #default="{ row }">
           <div class="flex items-center gap-2">
             <el-avatar
               class="shrink-0"
-              size="small"
               :style="{ backgroundColor: getAvatarColor(row.name) }"
+              :size="30"
             >
               {{ row.name?.charAt(0).toUpperCase() }}
             </el-avatar>
-            <span class="font-semibold text-sm line-clamp-1">{{
-              row.name
-            }}</span>
+            <div>
+              <span class="font-semibold text-sm line-clamp-1">
+                {{ row.name }}
+              </span>
+              <span class="text-xs text-gray-400 line-clamp-1">
+                {{ row.position }}
+              </span>
+            </div>
           </div>
         </template>
       </el-table-column>
@@ -46,33 +52,33 @@
       <el-table-column label="Customer" prop="Customer.name" min-width="150">
         <template #default="{ row }">
           <el-link :href="`/crm/customers/${row.Customer?.id}`" target="_blank">
-            {{ row.Customer?.name }}
+            <el-icon>
+              <ElIconOfficeBuilding />
+            </el-icon>
+            <span class="ml-1">
+              {{ row.Customer?.name }}
+            </span>
           </el-link>
         </template>
       </el-table-column>
 
-      <el-table-column label="Phone" prop="phone" min-width="150">
+      <el-table-column label="Contact" prop="phone" min-width="150">
         <template #default="{ row }">
+          <el-link :href="`mailto:${row.email}`">
+            {{ row.email }}
+          </el-link>
+          <br />
           <el-link
             v-if="row.phone"
             :href="`https://wa.me/${row.phone.replace(/[^0-9]/g, '')}`"
             target="_blank"
+            class="text-gray-400!"
           >
             {{ row.phone }}
           </el-link>
           <span v-else>-</span>
         </template>
       </el-table-column>
-
-      <el-table-column label="Email" prop="email" min-width="150">
-        <template #default="{ row }">
-          <el-link :href="`mailto:${row.email}`">
-            {{ row.email }}
-          </el-link>
-        </template>
-      </el-table-column>
-
-      <el-table-column label="Position" prop="position" min-width="150" />
 
       <el-table-column label="Note" prop="note" />
 
