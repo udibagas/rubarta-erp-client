57c779beee731696576673358fb54fb0fc2cb479
udibagas <udibagas@gmail.com>
2026-09-15 14:50:20 +0700

    fix search purchase order

--- pages/purchasing-logistics/purchase-orders/index.vue
+++ pages/purchasing-logistics/purchase-orders/index.vue
@@ -7,17 +7,13 @@
             <el-input
               v-model="keyword"
               placeholder="Search"
-              @change="refreshData()"
+              @change="refetch()"
               clearable
               :prefix-icon="ElIconSearch"
             />
 
             <el-button :icon="ElIconPlus" type="success" @click="openForm()" />
-            <el-button
-              @click="refreshData()"
-              :icon="ElIconRefresh"
-              class="ml-0!"
-            />
+            <el-button @click="refetch()" :icon="ElIconRefresh" class="ml-0!" />
           </div>
         </template>
       </el-page-header>
@@ -113,9 +109,8 @@
 definePageMeta({ layout: false });
 
 const quotationFormRef = ref(null);
-const keyword = ref("");
 
-const { fetchData, refreshData } = useCrud({
+const { fetchData, keyword } = useCrud({
   url: "/api/purchase-orders",
   queryKey: "purchase-orders",
 });
