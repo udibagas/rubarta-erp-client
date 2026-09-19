3b98de3ee0a6114cfb83120351eb5c39497101a6
udibagas <udibagas@gmail.com>
2026-09-19 20:46:58 +0700

    apply pagination on po

--- pages/purchasing-logistics/purchase-orders/index.vue
+++ pages/purchasing-logistics/purchase-orders/index.vue
@@ -10,6 +10,7 @@
               @change="refetch()"
               clearable
               :prefix-icon="ElIconSearch"
+              class="w-50!"
             />
 
             <el-button :icon="ElIconPlus" type="success" @click="openForm()" />
@@ -19,11 +20,21 @@
       </el-page-header>
     </template>
 
-    <el-table stripe v-loading="isPending" :data="data">
+    <el-table
+      stripe
+      v-loading="isPending"
+      :data="data?.data ?? []"
+      height="calc(100vh - 195px)"
+    >
       <template #empty>
         <el-empty description="No Items"> </el-empty>
       </template>
-      <el-table-column label="PO Number" prop="number" min-width="150">
+      <el-table-column
+        label="PO Number"
+        prop="number"
+        min-width="150"
+        fixed="left"
+      >
         <template #default="{ row }">
           <el-link
             class="font-mono font-semibold!"
@@ -104,23 +115,38 @@
       </el-table-column>
     </el-table>
 
-    <PurchaseOrderForm ref="quotationFormRef" @saved="() => refetch()" />
+    <el-pagination
+      class="p-2 bg-slate-100"
+      v-if="data?.total"
+      :current-page="page"
+      size="small"
+      background
+      layout="total, sizes, prev, pager, next"
+      :page-size="pageSize"
+      :page-sizes="[10, 25, 50, 100]"
+      :total="data?.total"
+      @current-change="currentChange"
+      @size-change="sizeChange"
+    />
+
+    <PurchaseOrderForm ref="purchaseOrderFormRef" @saved="() => refetch()" />
   </nuxt-layout>
 </template>
 
 <script setup>
 definePageMeta({ layout: false });
 
-const quotationFormRef = ref(null);
+const purchaseOrderFormRef = ref(null);
 
 const { fetchData, keyword } = useCrud({
   url: "/api/purchase-orders",
   queryKey: "purchase-orders",
 });
 
-const { isPending, data, refetch } = fetchData();
+const { isPending, data, refetch, page, pageSize, currentChange, sizeChange } =
+  fetchData();
 
 const openForm = (data = {}) => {
-  quotationFormRef.value?.openForm(data);
+  purchaseOrderFormRef.value?.openForm(data);
 };
 </script>
